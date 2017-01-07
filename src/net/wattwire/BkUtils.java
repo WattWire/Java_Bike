511aa5bd235307bea65e773e9ab1726167c47a6e
Dave Watt <gonebikn@gmail.com>
2017-01-07 14:16:53 -0800

    Boulanger w/ new BkUtils...

---
+++ src/net/wattwire/BkUtils.java
@@ -0,0 +1,18 @@
+package net.wattwire;
+
+/**
+ * Created by dave on 1/7/17.
+ */
+public class BkUtils {
+
+    // **** Print String Out, preceeded by nl line feeds:
+    public static void pstr(int nl, String s) {
+
+        for (; nl>0; --nl) {s = "\n"+s;}
+        System.out.print(s);
+
+    }
+
+
+
+}
