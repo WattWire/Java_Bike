bfd6db4d06127319e86b65e53f80dc12b9c6712e
Dave Watt <gonebikn@gmail.com>
2017-01-07 17:56:46 -0800

    Boulanger w/ new Bike Testing Classes

--- src/net/wattwire/dataModel/BikeColor.java
+++ src/net/wattwire/dataModel/BikeColor.java
@@ -9,14 +9,15 @@ public enum BikeColor {
     YELLOW("Yellow", 0xFFFF00),
     RED("Red",0xFF0000),
     BLACK("Black", 0x000000),
-    WHITE("White", 0xFFFFFF);
+    WHITE("White", 0xFFFFFF),
+    BLUE("Blue", 0x0000FF);
 
     String colorName;
     long colorVal;
 
     // *** Constructor:
 
-    BikeColor (String colorName, long colorVal) {
+    BikeColor(String colorName, long colorVal) {
 
         this.colorName = colorName;
         this.colorVal = colorVal;
