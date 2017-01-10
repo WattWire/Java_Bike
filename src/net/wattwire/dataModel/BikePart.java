f5b38e6bd1d9bc558402c4810dc98a22876a43f3
Dave Watt <gonebikn@gmail.com>
2017-01-09 18:04:32 -0800

    Monday Night & PopTart is Tired

--- src/net/wattwire/dataModel/BikePart.java
+++ src/net/wattwire/dataModel/BikePart.java
@@ -4,4 +4,42 @@ package net.wattwire.dataModel;
  * Created by dave on 1/8/17.
  */
 public class BikePart {
+
+    private int partNumber;
+    private String description;
+    private float price;
+
+    public BikePart(int partNumber, String description, float price) {
+
+        this.partNumber = partNumber;
+        this.description = description;
+        this.price = price;
+    }
+//----------------------------------------------------------------------
+
+    public int getPartNumber() {
+        return partNumber;
+    }
+
+    public void setPartNumber(int partNumber) {
+        this.partNumber = partNumber;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+
 }
